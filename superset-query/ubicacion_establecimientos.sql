fc6622396c2600277fb3670bb4be811c461843d1
Agustin Liendo Ortiz <152560063+AgusLiendo@users.noreply.github.com>
2025-05-20 11:36:54 -0300

    Update ubicacion_establecimientos.sql

--- superset-query/ubicacion_establecimientos.sql
+++ superset-query/ubicacion_establecimientos.sql
@@ -1,4 +1,10 @@
-SELECT nombre, lon , lat
+SELECT provincia.nombre AS nombre_provincia, departamento.nombre AS nombre_departamento , lon , lat, clae6_desc
 FROM public.distribucion_establecimientos
 INNER JOIN public.provincia
-ON distribucion_establecimientos.provincia_id = provincia.id;  
+ON distribucion_establecimientos.provincia_id = provincia.id
+
+INNER JOIN public.actividades_establecimientos
+ON distribucion_establecimientos.clae6 = actividades_establecimientos.clae6
+
+INNER JOIN public.departamento 
+ON distribucion_establecimientos.in_departamentos = departamento.id; 
